6d0d337aa1bd0f72a49d4e1d16a5302e1a83ba20
FireNaruto3 <jonathanlin2020@gmail.com>
2026-07-15 22:45:22 -0700

    nvim: include hidden files in pickers

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -9,7 +9,13 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          files = { hidden = true },
+          grep = { hidden = true },
+        },
+      },
       notifier = { enabled = true },
       input = { enabled = true },
     },
